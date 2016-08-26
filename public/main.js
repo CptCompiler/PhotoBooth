face5b4dba3a25e226cb6ab087ab9cb62fc7b985
Andres Stennert <andres@21stdigital.de>
2016-08-27 01:28:35 +0200

    update

--- public/main.js
+++ public/main.js
@@ -1,24 +1,27 @@
 var socket = io();
 
 
-
-$(document).ready(function(){
-  var owlContainer = $('.owl-carousel');
-  var owl = owlContainer.owlCarousel({
-    loop:false,
-    margin:10,
-    nav:true,
-    items: 1
-  });
-
-  owl.on('change.owl.carousel', function() {
-    console.log('changed');
-  });
-
-
-  socket.on('new photo', function(msg){
-    console.log('message: ' + msg);
-    owl.trigger('add.owl.carousel', ['<img src="photos/' + msg + '">', 0]);
-  });
-  
+socket.on('new photo', function(msg){
+  $('.image').attr('src', msg);
 });
+
+// $(document).ready(function(){
+//   var owlContainer = $('.owl-carousel');
+//   var owl = owlContainer.owlCarousel({
+//     loop:false,
+//     margin:10,
+//     nav:true,
+//     items: 1
+//   });
+
+//   owl.on('change.owl.carousel', function() {
+//     console.log('changed');
+//   });
+
+
+//   socket.on('new photo', function(msg){
+//     console.log('message: ' + msg);
+//     owl.trigger('add.owl.carousel', ['<img src="photos/' + msg + '">', 0]);
+//   });
+  
+// });
