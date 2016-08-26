8f567a535ee9acb02fab32a9db9d6177eee67039
Andres Stennert <andres@21stdigital.de>
2016-08-26 23:10:25 +0200

    update

--- server.js
+++ server.js
@@ -57,8 +57,24 @@ GPhoto.list(function (list) {
   // });
 
   // // Take picture with camera object obtained from list()
-   camera.takePicture({download: true}, function (er, data) {
-     fs.writeFileSync(__dirname + '/public/photos/' + new Date() + '.jpg', data);
+  camera.takePicture({download: true}, function (er, data) {
+    var path = __dirname + '/public/photos/' + new Date() + '.jpg';
+    fs.writeFileSync(path, data);
+    require('lwip').open(path, function(err, image){
+
+      // check err...
+      // define a batch of manipulations and save to disk as JPEG:
+      image.batch()
+        .scale(0.25)          // scale to 75%
+        //.rotate(45, 'white')  // rotate 45degs clockwise (white fill)
+        //.crop(200)            // crop a 200X200 square from center
+        //.blur(5)              // Gaussian blur with SD=5
+        .writeFile(path + '_small', function(err){
+          // check err...
+          // done.
+        });
+
+    });
    });
 
   // // Take picture without downloading immediately
