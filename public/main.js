0dc6e15570d1281862ef2963120dd48bab397336
Andres Stennert <andres@21stdigital.de>
2016-08-27 09:26:06 +0200

    update

--- public/main.js
+++ public/main.js
@@ -5,23 +5,23 @@ socket.on('new photo', function(msg){
   $('.image').attr('src', msg);
 });
 
-// $(document).ready(function(){
-//   var owlContainer = $('.owl-carousel');
-//   var owl = owlContainer.owlCarousel({
-//     loop:false,
-//     margin:10,
-//     nav:true,
-//     items: 1
-//   });
+$(document).ready(function(){
+  var owlContainer = $('.owl-carousel');
+  var owl = owlContainer.owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    items: 1
+  });
 
-//   owl.on('change.owl.carousel', function() {
-//     console.log('changed');
-//   });
+  owl.on('change.owl.carousel', function() {
+    console.log('changed');
+  });
 
 
-//   socket.on('new photo', function(msg){
-//     console.log('message: ' + msg);
-//     owl.trigger('add.owl.carousel', ['<img src="photos/' + msg + '">', 0]);
-//   });
+  socket.on('new photo', function(msg){
+    console.log('message: ' + msg);
+    owl.trigger('add.owl.carousel', ['<img src="photos/' + msg + '">', 0]);
+  });
   
-// });
+});
