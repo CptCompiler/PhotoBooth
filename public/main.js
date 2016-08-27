aa4ab3f1458d517a1af6f1253fd1e109e74066e1
Andres Stennert <andres@21stdigital.de>
2016-08-27 22:53:38 +0200

    update

--- public/main.js
+++ public/main.js
@@ -20,12 +20,15 @@ setInterval(function() {
 
 function setNewImage(imageIndex) {
   currentImageDiv.addClass('fadeOut');
-  currentImageDiv.css('-webkit-transform', 'translateY(-50%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(0.5)');
+  //currentImageDiv.css('-webkit-transform', 'translateY(-50%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(0.5)');
+  currentImageDiv.attr('style', '-webkit-transform: translateY(-50%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(0.5)');
+
   currentImageDiv.removeClass('fadeIn');
   nextImageDiv.html('<img src="photos/' + images[imageIndex] + '">');
   nextImageDiv.removeClass('fadeOut');
   nextImageDiv.addClass('fadeIn');
-  nextImageDiv.css('-webkit-transform', 'translateY(-50%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(1)');
+  //nextImageDiv.css('-webkit-transform', 'translateY(-50%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(1)');
+  nextImageDiv.attr('style', '-webkit-transform: translateY(-50%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(1)');
   var temp = currentImageDiv;
   currentImageDiv = nextImageDiv;
   nextImageDiv = temp;
