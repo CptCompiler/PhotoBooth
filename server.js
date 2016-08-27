57775e8c7f180f6eb5b3652c2c5fb91b4b06020f
Andres Stennert <andres@21stdigital.de>
2016-08-27 09:54:20 +0200

    update

--- server.js
+++ server.js
@@ -65,56 +65,30 @@ GPhoto.list(function (list) {
   //   //...
   // });
 
-  // // Take picture with camera object obtained from list()
-  camera.takePicture({download: true}, function (er, data) {
-    var fileName = new Date();
-    var path = __dirname + '/public/photos/';
-    fs.writeFileSync(path + fileName + '.jpg', data);
-    io.emit('new photo', fileName + '.jpg');
-    // require('lwip').open(path + fileName + '.jpg', function(err, image){
-    //   console.log(err);
-    //   // check err...
-    //   // define a batch of manipulations and save to disk as JPEG:
-    //   image.batch()
-    //     .scale(0.1)          // scale to 75%
-    //     //.rotate(45, 'white')  // rotate 45degs clockwise (white fill)
-    //     //.crop(200)            // crop a 200X200 square from center
-    //     //.blur(5)              // Gaussian blur with SD=5
-    //     .writeFile(path + fileName + '_small.jpg', function(err){
-    //       // check err...
-    //       // done.
-    //       console.log(err);
 
-    //     });
+  setInterval(function {
+    camera.takePicture({download: true}, function (er, data) {
+      var fileName = new Date();
+      var path = __dirname + '/public/photos/';
+      fs.writeFileSync(path + fileName + '.jpg', data);
+      io.emit('new photo', fileName + '.jpg');
+      // require('lwip').open(path + fileName + '.jpg', function(err, image){
+      //   console.log(err);
+      //   // check err...
+      //   // define a batch of manipulations and save to disk as JPEG:
+      //   image.batch()
+      //     .scale(0.1)          // scale to 75%
+      //     //.rotate(45, 'white')  // rotate 45degs clockwise (white fill)
+      //     //.crop(200)            // crop a 200X200 square from center
+      //     //.blur(5)              // Gaussian blur with SD=5
+      //     .writeFile(path + fileName + '_small.jpg', function(err){
+      //       // check err...
+      //       // done.
+      //       console.log(err);
 
-    // });
-   });
+      //     });
 
-  // // Take picture without downloading immediately
-  // camera.takePicture({download: false}, function (er, path) {
-  //   console.log(path);
-  // });
-
-  // Take picture and download it to filesystem
-//  camera.takePicture({
-  //  targetPath: '/tmp/foo.XXXXXX'
- // }, function (er, tmpname) {
- //   fs.renameSync(tmpname, __dirname + 'public/photos/' + 123  + '.jpg');
- // });
-
-  // // Download a picture from camera
-  // camera.downloadPicture({
-  //   cameraPath: '/store_00020001/DCIM/100CANON/IMG_1231.JPG',
-  //   targetPath: '/tmp/foo.XXXXXX'
-  // }, function (er, tmpname) {
-  //   fs.renameSync(tmpname, __dirname + '/picture.jpg');
-  // });
-
-  // // Get preview picture (from AF Sensor, fails silently if unsupported)
-  // camera.takePicture({
-  //   preview: true,
-  //   targetPath: '/tmp/foo.XXXXXX'
-  // }, function (er, tmpname) {
-  //   fs.renameSync(tmpname, __dirname + '/picture.jpg');
-  // });
+      // });
+    });
+  }, 5000);
 });
