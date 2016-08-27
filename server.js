32d400b529b73747916138423c0f8c13d9548cd7
Andres Stennert <andres@21stdigital.de>
2016-08-27 22:13:46 +0200

    update

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ function takePicture() {
       
       var epeg = require("epeg");
       var image = new epeg.Image({data: data});
-      buffer = image.downsize(100, 100).process();
+      buffer = image.downsize(1200, 800).process();
       fs.writeFileSync(path + fileName + '.jpg', buffer);
       
       io.emit('new photo', fileName + '.jpg');
