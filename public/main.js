227748466df230ebc5a82b4376ec2bcca68ea485
Andres Stennert <andres@21stdigital.de>
2016-08-28 10:43:20 +0200

    update

--- public/main.js
+++ public/main.js
@@ -5,6 +5,7 @@ var socket = io();
 var images = $('.images').data('images');
 var currentImageDiv = $('.imageOne');
 var nextImageDiv = $('.imageTwo');
+var overlay = $('.overlay');
 currentImageDiv.html('<img src="photos/' + images[0] + '">');
 
 socket.on('new photo', function(msg){
@@ -19,18 +20,28 @@ setInterval(function() {
 
 
 function setNewImage(imageIndex) {
-  currentImageDiv.addClass('fadeOut');
+  //currentImageDiv.addClass('fadeOut');
   //currentImageDiv.css('-webkit-transform', 'translateY(-50%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(0.5)');
   //currentImageDiv.attr('style', '-webkit-transform: translateY(150%) ');
 
-  currentImageDiv.removeClass('fadeIn');
-  nextImageDiv.attr('src', 'photos/' + images[imageIndex]);
+  //currentImageDiv.removeClass('fadeIn');
+
+  overlay.addClass('visible');
+  setTimeout(function() {
+    currentImageDiv.attr('src', 'photos/' + images[imageIndex]);
+    
+    setTimeout(function() {
+      overlay.removeClass('visible');
+    }, 2000);
+  }, 1000);
   
-  nextImageDiv.removeClass('fadeOut');
-  nextImageDiv.addClass('fadeIn');
+  
+  
+  //nextImageDiv.removeClass('fadeOut');
+  //nextImageDiv.addClass('fadeIn');
   //nextImageDiv.css('-webkit-transform', 'translateY(-50%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(1)');
   //nextImageDiv.attr('style', '-webkit-transform: translateY(-50%) ');
-  var temp = currentImageDiv;
-  currentImageDiv = nextImageDiv;
-  nextImageDiv = temp;
+  //var temp = currentImageDiv;
+  //currentImageDiv = nextImageDiv;
+  //nextImageDiv = temp;
 }
