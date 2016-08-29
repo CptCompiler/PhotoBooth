8c032854f4c6b3aa6dd28d4751529551c4828639
Andres Stennert <andres@21stdigital.de>
2016-08-30 00:18:25 +0200

    update

--- public/main.js
+++ public/main.js
@@ -6,7 +6,7 @@ var images = $('.images').data('images');
 var currentImageDiv = $('.imageOne');
 var nextImageDiv = $('.imageTwo');
 var overlay = $('.overlay');
-var overlayText = $('.overlay .text');
+var bgText = $('.bg .text');
 currentImageDiv.html('<img src="photos/' + images[0] + '">');
 
 var slideshowInterval = null;
@@ -17,8 +17,8 @@ function activateSlideshow() {
   slideshowTimer = setTimeout(function() {
     slideshowInterval = setInterval(function() {
       setNewImage(Math.floor(Math.random() * images.length));
-    }, 4000);
-  }, 1000);
+    }, 6000);
+  }, 20000);
 }
 
 function deactivateSlideshow() {
@@ -34,29 +34,30 @@ function deactivateSlideshow() {
 
 
 
-// socket.on('new photo', function(msg){
-//   $('.image').attr('src', 'photos/' + msg);
-//   images.push(msg);
-//   setNewImage(images.length-1);
-//   activateSlideshow();
-// });
+socket.on('new photo', function(msg){
+  $('.image').attr('src', 'photos/' + msg);
+  images.push(msg);
+  setNewImage(images.length-1);
+  activateSlideshow();
+});
 
-// socket.on('taking photo', function(msg){
-//   deactivateSlideshow();
-//   overlay.addClass('visible');
-//   setTimeout(function() { 
-//     overlayText.text('3');
-//     setTimeout(function() { 
-//       overlayText.text('2');
-//       setTimeout(function() { 
-//         overlayText.text('1');
-//         setTimeout(function() { 
-//           overlayText.text('cheese');
-//         }, 1000);
-//       }, 1000);
-//     }, 1000);
-//   }, 1000);
-// });
+socket.on('taking photo', function(msg){
+  deactivateSlideshow();
+  currentImageDiv.addClass('fadeOut');
+  currentImageDiv.attr('style', '-webkit-transform: translate(' + ((Math.floor((Math.random() * 10) % 2) - 1) * 100) + '%, '  + (Math.random() * 200 -100) + '%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(0)');
+  setTimeout(function() { 
+    bgText.text('3');
+    setTimeout(function() { 
+      bgText.text('2');
+      setTimeout(function() { 
+        bgText.text('1');
+        setTimeout(function() { 
+          bgText.text('cheese');
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 2000);
+});
 
 
 
@@ -64,7 +65,6 @@ function deactivateSlideshow() {
 
 function setNewImage(imageIndex) {
   currentImageDiv.addClass('fadeOut');
-  //currentImageDiv.css('-webkit-transform', 'translateY(-50%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(0.5)');
   currentImageDiv.attr('style', '-webkit-transform: translate(' + ((Math.floor((Math.random() * 10) % 2) - 1) * 100) + '%, '  + (Math.random() * 200 -100) + '%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(0)');
 
   currentImageDiv.removeClass('fadeIn');
