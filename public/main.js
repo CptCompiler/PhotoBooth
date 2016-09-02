a6890926e1e84e46f472c15d814386a411f675ac
Andres Stennert <andres@21stdigital.de>
2016-09-02 09:04:12 +0200

    update

--- public/main.js
+++ public/main.js
@@ -17,37 +17,40 @@ $('.photoBtn').click(function() {
   socket.emit('take photo');
 });
 
-activateSlideshow();
-function activateSlideshow() {
-  slideshowTimer = setTimeout(function() {
-    slideshowInterval = setInterval(function() {
-      setNewImage(Math.floor(Math.random() * images.length));
-    }, 6000);
-  }, 30000);
-}
+// activateSlideshow();
+// function activateSlideshow() {
+//   slideshowTimer = setTimeout(function() {
+//     slideshowInterval = setInterval(function() {
+//       setNewImage(Math.floor(Math.random() * images.length));
+//     }, 6000);
+//   }, 30000);
+// }
 
-function deactivateSlideshow() {
-  if (slideshowTimer) {
-    clearTimeout(slideshowTimer);
-    slideshowTimer = null;
-  }
-  if (slideshowInterval) {
-    clearInterval(slideshowInterval);
-    slideshowInterval = null;
-  }
-}
+// function deactivateSlideshow() {
+//   if (slideshowTimer) {
+//     clearTimeout(slideshowTimer);
+//     slideshowTimer = null;
+//   }
+//   if (slideshowInterval) {
+//     clearInterval(slideshowInterval);
+//     slideshowInterval = null;
+//   }
+// }
 
-
+socket.on('show photo', function(msg){
+  setNewImage(msg);
+  //activateSlideshow();
+});
 
 socket.on('new photo', function(msg){
   $('.image').attr('src', 'photos/' + msg);
   images.push(msg);
   setNewImage(images.length-1);
-  activateSlideshow();
+  //activateSlideshow();
 });
 
 socket.on('taking photo', function(msg){
-  deactivateSlideshow();
+  //deactivateSlideshow();
   currentImageDiv.addClass('fadeOut');
   currentImageDiv.attr('style', '-webkit-transform: translate(' + ((Math.floor((Math.random() * 10) % 2) - 1) * 100) + '%, '  + (Math.random() * 200 -100) + '%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(0)');
   setTimeout(function() { 
@@ -68,12 +71,12 @@ socket.on('taking photo', function(msg){
 
 
 
-function setNewImage(imageIndex) {
+function setNewImage(imageFileName) {
   currentImageDiv.addClass('fadeOut');
   currentImageDiv.attr('style', '-webkit-transform: translate(' + ((Math.floor((Math.random() * 10) % 2) - 1) * 100) + '%, '  + (Math.random() * 200 -100) + '%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(0)');
 
   currentImageDiv.removeClass('fadeIn');
-  nextImageDiv.html('<img src="photos/' + images[imageIndex] + '">');
+  nextImageDiv.html('<img src="photos/' + imageFileName + '">');
   nextImageDiv.removeClass('fadeOut');
   nextImageDiv.addClass('fadeIn');
   //nextImageDiv.css('-webkit-transform', 'translateY(-50%) rotateZ(' + (Math.random() * 20 -10) + 'deg) scale(1)');
--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const path = require('path');
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.static('public'));
+
+var photos = fs.readdirSync(__dirname + '/public/photos/').filter(function(e) {
+  return e.indexOf('.jpg') != -1;
+});
+
+var takingPhoto = false;
 
 app.get('/', function (req, res) {
   res.render('index',
@@ -38,7 +45,7 @@ http.listen(3000, function(){
 
 var gphoto2 = require('gphoto2');
 var GPhoto = new gphoto2.GPhoto2();
-var fs = require('fs');
+
 
 var camera;
 // List cameras / assign list item to variable to use below options
@@ -50,35 +57,65 @@ GPhoto.list(function (list) {
 });
 
 function takePicture() {
-  //setInterval(function() {
-    io.emit('taking photo');
-    setTimeout(function() {
-      camera.takePicture({download: true}, function (er, data) {
-        var fileName = new Date();
-        var path = __dirname + '/public/photos/';
-        fs.writeFileSync('archive/' + fileName + '.jpg', data);
-        
-        var epeg = require("epeg");
-        var image = new epeg.Image({data: data});
-        buffer = image.downsize(1280, 900).process();
-        fs.writeFileSync(path + fileName + '.jpg', buffer);
-        
-        io.emit('new photo', fileName + '.jpg');
-      });
-      //io.emit('new photo', 'test.jpg');
-    }, 5000);
-  //}, 60000);
+  if (takingPhoto) {
+    return;
+  }
+  takingPhoto = true;
+  deactivateSlideshow();
+  io.emit('taking photo');
+  setTimeout(function() {
+    camera.takePicture({download: true}, function (er, data) {
+      var fileName = new Date();
+      var path = __dirname + '/public/photos/';
+      fs.writeFileSync('archive/' + fileName + '.jpg', data);
+      
+      var epeg = require("epeg");
+      var image = new epeg.Image({data: data});
+      buffer = image.downsize(1280, 900).process();
+      fs.writeFileSync(path + fileName + '.jpg', buffer);
+      photos.push(fileName + '.jpg');
+      io.emit('new photo', fileName + '.jpg');
+      activateSlideshow();
+      takingPhoto = false;
+    });
+  }, 5000);
 }
 
 
-var Gpio = require('pigpio').Gpio;
-var button = new Gpio(4, {
-  mode: Gpio.INPUT,
-  pullUpDown: Gpio.PUD_DOWN,
-  edge: Gpio.EITHER_EDGE
-});
 
-button.on('interrupt', function (level) {
-  console.log('button pressed');
-});
+var slideshowInterval = null;
+var slideshowTimer = null;
+
+
+activateSlideshow();
+function activateSlideshow() {
+  slideshowTimer = setTimeout(function() {
+    slideshowInterval = setInterval(function() {
+      io.emit('show photo', photos[Math.floor(Math.random() * photos.length)]);
+    }, 6000);
+  }, 30000);
+}
+
+function deactivateSlideshow() {
+  if (slideshowTimer) {
+    clearTimeout(slideshowTimer);
+    slideshowTimer = null;
+  }
+  if (slideshowInterval) {
+    clearInterval(slideshowInterval);
+    slideshowInterval = null;
+  }
+}
+
+
+// var Gpio = require('pigpio').Gpio;
+// var button = new Gpio(4, {
+//   mode: Gpio.INPUT,
+//   pullUpDown: Gpio.PUD_DOWN,
+//   edge: Gpio.EITHER_EDGE
+// });
+
+// button.on('interrupt', function (level) {
+//   console.log('button pressed');
+// });
 
